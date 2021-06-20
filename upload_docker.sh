73a47c450cf79e00564bc9cff159921909e138c4
Damla Beyaz <ddurmaz88@gmail.com>
2021-06-20 20:25:15 +0200

    Pushes docker image to DockerHub

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
 
-# Assumes that an image is built via `run_docker.sh`
+dockerpath=damlabeyaz/udacity_project4
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+docker login
+docker tag udacity_project4 $dockerpath
 
-# Step 2:  
-# Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
-# Push image to a docker repository
+docker push $dockerpath
